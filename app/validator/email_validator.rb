b0f0546e3f49dd13aef31e80858027314caf6c4f
snsh <ccccportal@yahoo.co.jp>
2015-05-29 01:12:23 +0900

    first commit

--- app/validator/email_validator.rb
+++ app/validator/email_validator.rb
@@ -1,13 +1,12 @@
 class EmailValidator < ActiveModel::Validator
 
   VALID_REGEX = /^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/
-  LENGTH = 150
   def validate(record)
 
     if record.email.blank?
       record.errors[:email] << ' can\'t be blank'
     else
-      unless record.email.length <= LENGTH
+      unless record.email.length <= 150
         record.errors[:email] << " is the wrong length (should be 150 characters)"
       end
       unless record.email =~ VALID_REGEX
